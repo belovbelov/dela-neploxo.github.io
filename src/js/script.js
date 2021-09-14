0538663d33d8cbab4c4f631682d4b572636a7c42
dela-neploxo <itoha32@gmail.com>
2021-09-14 21:15:51 +0300

    Add validation for Tests page

--- src/js/script.js
+++ src/js/script.js
@@ -16,3 +16,28 @@ function showImages() {
       .append(image, imageName);
   }
 }
+
+function wordCounter(text) {
+  text = text.split(" ");
+  let wordCount = 0;
+  for (let i = 0; i < text.length; i++) {
+    if (text[i] !== " ") {
+      wordCount++;
+    }
+  }
+  return wordCount;
+}
+
+function validateTest(tag) {
+  let words = wordCounter(tag.value);
+  if (words < 30) {
+    tag.setCustomValidity("НЕТ");
+  }
+}
+
+function validateName(tag) {
+  let words = wordCounter(tag.value);
+  if (words < 2 || words > 2) {
+    tag.setCustomValidity("НЕТ");
+  }
+}
